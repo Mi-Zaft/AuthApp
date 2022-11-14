37c0e15820531fbb83455a42197bedcaad017e93
MiZaft <mi.zaft1@gmail.com>
2022-11-14 18:28:38 +0300

    Initial Commit

---
+++ AuthApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthApp
+//
+//  Created by Максим Евграфов on 14.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
