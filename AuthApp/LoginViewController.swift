18f95cd324c721f9e2c147e9ffb6d12bae98d842
MiZaft <mi.zaft1@gmail.com>
2022-11-21 22:59:22 +0300

    Finish

--- AuthApp/LoginViewController.swift
+++ AuthApp/LoginViewController.swift
@@ -8,12 +8,63 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    private let userName = "Root"
+    private let password = "123456"
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.userName = userName
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
 
-
+    @IBAction func loginTapped() {
+        if userNameTextField.text == userName && passwordTextField.text == password {
+            performSegue(withIdentifier: "goWelcomeVC", sender: nil)
+        } else {
+            let alert = UIAlertController(
+                title:"Invalid auth!",
+                message: "Please, check your login and password",
+                preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "Ok", style: .default)
+            alert.addAction(alertAction)
+            present(alert, animated: true)
+            
+            passwordTextField.text = ""
+        }
+    }
+    
+    @IBAction func forgotUserNameTapped() {
+        let alert = UIAlertController(
+            title:"Oops!",
+            message: "Root",
+            preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(alertAction)
+        present(alert, animated: true)
+    }
+    
+    @IBAction func forgotPasswordTapped() {
+        let alert = UIAlertController(
+            title:"Oops!",
+            message: "123456",
+            preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(alertAction)
+        present(alert, animated: true)
+    }
+    
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
 }
 
---
+++ AuthApp/WelcomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeViewController.swift
+//  AuthApp
+//
+//  Created by Максим Евграфов on 21.11.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var userName = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        welcomeLabel.text = "Welcome, \(userName)"
+    }
+}
